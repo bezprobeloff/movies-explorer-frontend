7b0b682ddc8883b9055f1fc06466fc723ff6961c
Mikhail Belov <bezprobeloff@gmail.com>
2022-11-22 17:02:17 +0300

    feat: открытие/закрытие меню в мобилке

--- src/components/HeaderNavigation/HeaderNavigation.jsx
+++ src/components/HeaderNavigation/HeaderNavigation.jsx
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './HeaderNavigation.scss';
 
 const HeaderNavigation = () => {
+  const [isBurgerClose, setIsBurgerClose] = useState(true);
+  const classBurgerMenu = isBurgerClose
+    ? ' navigation__button-burger_type_close'
+    : '';
+  const classNavigationMenu = isBurgerClose ? ' navigation_type_menu' : '';
+  const classNavigationLinksMenu = isBurgerClose
+    ? ''
+    : ' navigation__links_invisibled';
+  const classNavigationAccountMenu = isBurgerClose
+    ? ''
+    : ' navigation__account_invisibled';
+
+  const onButtonBurgerClick = () => {
+    setIsBurgerClose((state) => !state);
+  };
+
+  useEffect(() => {
+    setIsBurgerClose(false);
+  }, []);
+
   return (
-    <div className='navigation'>
-      <button className='navigation__button-burger'></button>
-      <nav className='navigation__links'>
+    <div className={`navigation${classNavigationMenu}`}>
+      <button
+        className={`navigation__button-burger${classBurgerMenu}`}
+        onClick={onButtonBurgerClick}
+      ></button>
+      <nav className={`navigation__links${classNavigationLinksMenu}`}>
         <a className='navigation__link'>Главная</a>
         <a className='navigation__link'>Фильмы</a>
         <a className='navigation__link'>Сохранённые фильмы</a>
       </nav>
-      <a className='navigation__account'>Аккаунт</a>
+      <a className={`navigation__account${classNavigationAccountMenu}`}>
+        Аккаунт
+      </a>
     </div>
   );
 };
