cb0c423f828b482490eacc8268bbbf359d5a8e3a
Mikhail Belov <bezprobeloff@gmail.com>
2022-11-23 11:45:07 +0300

    feat: адаптация остальных секций под desktop

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,14 +4,16 @@ import './Footer.scss';
 const Footer = () => {
   return (
     <footer className='footer'>
-      <h2 className='footer__title'>
-        Учебный проект Яндекс.Практикум х BeatFilm.
-      </h2>
-      <nav className='footer__nav'>
-        <a className='footer__nav-link'>Яндекс.Практикум</a>
-        <a className='footer__nav-link'>Github</a>
-      </nav>
-      <p className='footer__copyright'>{`© ${new Date().getFullYear()}`}</p>
+      <div className='footer__container'>
+        <h2 className='footer__title'>
+          Учебный проект Яндекс.Практикум х BeatFilm.
+        </h2>
+        <nav className='footer__nav'>
+          <a className='footer__nav-link'>Яндекс.Практикум</a>
+          <a className='footer__nav-link'>Github</a>
+        </nav>
+        <p className='footer__copyright'>{`© ${new Date().getFullYear()}`}</p>
+      </div>
     </footer>
   );
 };
