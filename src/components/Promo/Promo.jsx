213b81dce6b96525be886547ab16646cdd352dc0
Mikhail Belov <bezprobeloff@gmail.com>
2022-11-27 16:57:19 +0300

    feat: сверстана promo для desktop

--- src/components/Promo/Promo.jsx
+++ src/components/Promo/Promo.jsx
@@ -6,13 +6,15 @@ const Promo = () => {
   return (
     <section className='promo'>
       <img src={logo} className='promo__logo' alt='Логотип WEB-планета' />
-      <h1 className='promo__title'>
-        Учебный проект студента факультета Веб-разработки.
-      </h1>
-      <p className='promo__text'>
-        Листайте ниже, чтобы узнать больше про этот проект и его создателя.
-      </p>
-      <a className='promo__link'>Узнать больше</a>
+      <div className='promo__info'>
+        <h1 className='promo__title'>
+          Учебный проект студента факультета Веб&#8209;разработки.
+        </h1>
+        <p className='promo__text'>
+          Листайте ниже, чтобы узнать больше про этот проект и его создателя.
+        </p>
+        <a className='promo__link'>Узнать больше</a>
+      </div>
     </section>
   );
 };
