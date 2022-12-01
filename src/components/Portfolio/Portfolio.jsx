523e56e34f0deca9704726c2e03a9394d08ba358
Mikhail Belov <bezprobeloff@gmail.com>
2022-12-01 09:16:18 +0300

    refact: список ссылок portfolio > ul-li

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -6,32 +6,38 @@ const Portfolio = () => {
   return (
     <section className='project portfolio'>
       <h3 className='portfolio__title'>Портфолио</h3>
-      <nav className='portfolio__links'>
-        <a
-          href='https://github.com/bezprobeloff/how-to-learn'
-          rel='noreferrer'
-          target='_blank'
-          className='link portfolio__link'
-        >
-          Статичный сайт
-        </a>
-        <a
-          href='https://github.com/bezprobeloff/russian-travel'
-          rel='noreferrer'
-          target='_blank'
-          className='link portfolio__link'
-        >
-          Адаптивный сайт
-        </a>
-        <a
-          href='https://github.com/bezprobeloff/react-mesto-api-full'
-          rel='noreferrer'
-          target='_blank'
-          className='link portfolio__link'
-        >
-          Одностраничное приложение
-        </a>
-      </nav>
+      <ul className='portfolio__links'>
+        <li className='portfolio__link-item'>
+          <a
+            href='https://github.com/bezprobeloff/how-to-learn'
+            rel='noreferrer'
+            target='_blank'
+            className='link portfolio__link'
+          >
+            Статичный сайт
+          </a>
+        </li>
+        <li className='portfolio__link-item'>
+          <a
+            href='https://github.com/bezprobeloff/russian-travel'
+            rel='noreferrer'
+            target='_blank'
+            className='link portfolio__link'
+          >
+            Адаптивный сайт
+          </a>
+        </li>
+        <li className='portfolio__link-item'>
+          <a
+            href='https://github.com/bezprobeloff/react-mesto-api-full'
+            rel='noreferrer'
+            target='_blank'
+            className='link portfolio__link'
+          >
+            Одностраничное приложение
+          </a>
+        </li>
+      </ul>
     </section>
   );
 };
