8fc1566e9bcc7b89ab696926a257465b654c4720
Mikhail Belov <bezprobeloff@gmail.com>
2023-04-05 11:37:06 +0300

    update api adress

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-const BASE_MAIN_URL = 'https://api.movies.bezprobeloff.nomoredomains.icu';
+const BASE_MAIN_URL = 'https://api.movies.bezprobeloff.ru';
 const BASE_IMAGE_URL = 'https://api.nomoreparties.co';
 const BASE_MOVIES_URL = 'https://api.nomoreparties.co/beatfilm-movies';
 const SHORT_DURATION = 40;
